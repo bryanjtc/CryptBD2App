ad270e8bd006e8f9df576752e8321bf796b234b6
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2021-11-08 14:20:44 -0500

    Added files

---
+++ Controllers/DataController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using ClassLibraryCryptoBD2;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CryptBD2App.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        Aes myAes = Aes.Create();
+        Data data = new();
+
+        // GET: <DataController>
+        [HttpGet]
+        public string[] Get()
+        {
+            string values = data.TextoEncriptado.ToString() + "," + data.TextoDesencriptado;
+            return values.Split(",");
+        }
+
+        // GET <DataController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST <DataController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            data.TextoIngresado = value;
+            byte[] encrypted = Crypt.EncryptStringToBytes_Aes(value, myAes.Key, myAes.IV);
+            data.TextoEncriptado = encrypted;
+            data.TextoDesencriptado = Crypt.DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+        }
+
+        // PUT <DataController>/
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+
+        }
+
+        // DELETE <DataController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Data.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptBD2App
+{
+    public class Data
+    {
+        public string TextoIngresado { get; set; }
+        public byte[] TextoEncriptado { get; set; }
+        public string TextoDesencriptado { get; set; }
+
+    }
+}
